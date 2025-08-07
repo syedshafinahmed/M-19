10fecdd90633d744c598b85edfae15caf26b4d19
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-07 23:43:03 +0600

    add

---
+++ return.js
@@ -0,0 +1,13 @@
+function tenTimes(number){
+    const result = number * 10;
+    return result;
+}
+
+
+function cutHalf(number){
+    const half = number / 2;
+    return half;
+}
+
+const output = tenTimes(5);
+console.log(output);
