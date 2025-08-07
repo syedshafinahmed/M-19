f854577c032f5f3165c3201e1ef70c19bf4b070b
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-07 20:50:52 +0600

    add

---
+++ parameter.js
@@ -0,0 +1,15 @@
+function bhaatKhao(){
+    console.log("hand wash kore asho");
+    console.log("bosho");
+    console.log("plate ee khabar nao");
+    console.log("khawa shuru koro");
+}
+// bhaatKhao();
+
+function square(x){
+    square = x * x;
+    return square;
+}
+
+let borgo = square(11);
+console.log(borgo);
